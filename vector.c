0b97f8a79d5ae557a9bf02612a11b97d94986abf
Fred <Ventag>
2018-05-22 01:55:46 +0200

    Cleanup 1.0

--- vector.c
+++ vector.c
@@ -4,13 +4,13 @@
 void printVector(vector_t *v)
 {
     if (v != NULL)
-    { //check if null before printing as printf with %i and others will result in undefined behaviour
+    {
         for (int i = 0; i < v->size; i++)
         {
             printf("vbuffer: [i: %u, element ptr: %p, element deref: %d], Size: %zu, Capacity: %zu\n",
                    i, &v->vbuffer[i], *(int *) v->vbuffer[i], v->size, v->capacity);
-            printf("actual size in bytes %zu\n", (v->size) * sizeof(void *)); //This is the actual size in bytes, the other is the spare room in bytes.
-            printf("total size in bytes %zu\n", v->capacity * sizeof(void *)); //just for extra debugging.
+            printf("actual size in bytes %zu\n", (v->size) * sizeof(void *));
+            printf("total size in bytes %zu\n", v->capacity * sizeof(void *));
         }
     }
     else
@@ -19,101 +19,84 @@ void printVector(vector_t *v)
     }
 }
 
-/*
-* Initialize a vector to be empty
-* Pre: 'vector' != NULL
-* Initialize the vector.
-* allocate and assign memory for the array and assign it's elements.
-*/
 void vector_init(vector_t *vector)
 {
     if (vector != NULL)
     {
-        vector->size = (size_t) 0; //reset size to 0;
-        vector->capacity = (size_t) 10; // set the initial capacity to 10;
-        vector->vbuffer = malloc(sizeof(void*) * vector->capacity); //allocate memory on the heap.
+        vector->size = (size_t) 0;
+        vector->capacity = (size_t) 10;
+        vector->vbuffer = malloc(sizeof(void*) * vector->capacity);
         if (vector->vbuffer == 0)
         {
             printf("Failed to allocate memory.\n");
-        } //failsafe for malloc, in case memory is full etc.
+        }
     }
 }
 
-// Deallocate internal structures of the vector
-// Note: the user is responsible for deleting all values
-// Note: the user is responsible for deleting the actual vector if it was dynamically allocated
 void vector_delete(vector_t *vector)
 {
     if (vector != NULL)
     {
-        free(vector->vbuffer);   // free the allocated memory
-        vector->capacity = 0;    // reset capacity
-        vector->size = 0;        // reset size
+        free(vector->vbuffer);
+        vector->capacity = 0;
+        vector->size = 0;
     }
 }
 
-// Insert a new element at the end of the vector
-// Pre: 'vector' != NULL
 void vector_push(vector_t *vector, void *value)
 {
     if (vector != NULL && value != NULL)
-    { // make sure the passed in parameters are valid
+    {
         if (vector->size >= vector->capacity)
         {
-            size_t newCapacity = vector->capacity * 2; //resize 2n
+            size_t newCapacity = vector->capacity * 2;
 
             if (newCapacity <= vector->capacity)
             {
                 printf("vector overflow.\n");
                 exit(1);
-            } // handle overflowing, if vector is deleted and someone pushes into it, this will trigger.
+            }
 
-            vector->vbuffer = realloc(vector->vbuffer, sizeof(void *) * newCapacity); // realloc is nice :)
+            vector->vbuffer = realloc(vector->vbuffer, sizeof(void *) * newCapacity);
 
             if (vector->vbuffer == NULL)
             {
                 printf("failed to allocate memory for vector resize.\n");
-            } // handle realloc failure.
+            }
 
-            vector->capacity = newCapacity; //update to the new capacity
+            vector->capacity = newCapacity;
         }
-        vector->vbuffer[vector->size] = value; // update the last element in the vector to the value given.
-        vector->size++; // increment the size for future pushes.
+        vector->vbuffer[vector->size] = value;
+        vector->size++;
     }
 }
 
-// Remove the last element in the vector and return the value
-// Pre: the vector is non-empty, 'vector' != NULL
 void *vector_pop(vector_t *vector)
 {
     if (vector != NULL &&
         vector->size >= 1)
-    { // first we check if the vector is not null and that the size of the vector is valid.
+    {
         void *element = vector->vbuffer[vector->size -
-                                        1]; // put void * element on the stack and retrieve the last element from our buffer and assign it to element.
-        vector->size = vector->size - 1; //decrement the size since we've popped it.
-        return element; // return to user.
+                                        1];
+        vector->size = vector->size - 1;
+        return element;
     }
 }
 
-// Return the number of elements in the vector
-// Pre: 'vector' != NULL
 size_t vector_size(const vector_t *vector)
 {
-    if (vector != NULL) //check for null
-        return vector->size; // return to user.
+    if (vector != NULL)
+        return vector->size;
     else
     {
         exit(1);
     }
 }
 
-// Return the current capacity of the vector
-// Pre: 'vector' != NULL
 size_t vector_capacity(const vector_t *vector)
 {
-    if (vector != NULL) //check for null
-        return vector->capacity; //return to user
+    if (vector != NULL)
+        return vector->capacity;
     else
     {
         printf("Vector is null or not initialized.\n");
@@ -121,24 +104,20 @@ size_t vector_capacity(const vector_t *vector)
     }
 }
 
-// Return the value at the given index
-// Pre: index < vector_size(vector)
 void *vector_get_element(const vector_t *vector, size_t index)
 {
     if (vector != NULL &&
-        index < vector->size) // check if the vector is null, and the given index is less than the vector size.
-        return vector->vbuffer[index]; //return the given index in the buffer.
+        index < vector->size)
+        return vector->vbuffer[index];
     else
     {
         return NULL;
     }
 }
 
-// Return a pointer to the underlying array
 void **vector_get_array(const vector_t *vector)
 {
     if (vector != NULL)
-    { // Not explicitly stated as a prerequisite. Should still check this though.
-        return vector->vbuffer; // return the actual.
+        return vector->vbuffer;
     }
 }
--- vector.h
+++ vector.h
@@ -3,50 +3,26 @@
 
 #include <stdlib.h>
 
-//-----------------------------------------------------------------------------
-// Vector, a dynamic array using size doubling to achieve amortized constant time insertion
-// There are prerequisites mentions in each function description. The user has to make sure
-// that these conditions are fulfilled before the function call. Nevertheless, the function
-// should check these conditions as well and react accordingly (e.g., exit the program)
-//-----------------------------------------------------------------------------
-
-// actually define the structs
 typedef struct vector_t {
-    size_t size;     // The size denotes the amount of elements currently occupied in the array. size_t was used because vector_capacity returns a size_t.  
-    size_t capacity; // The capacity denotes the maximum length of the array.
-    void **vbuffer;  // the vector buffer, this is our array and here we store our elements. Decided to use void** because that makes the most sense.
+    size_t size;
+    size_t capacity;
+    void **vbuffer;
 } vector_t;
 
-// Initialize a vector to be empty
-// Pre: 'vector' != NULL
 void vector_init(vector_t *vector);
 
-// Deallocate internal structures of the vector
-// Note: the user is responsible for deleting all values
-// Note: the user is responsible for deleting the actual vector if it was dynamically allocated
 void vector_delete(vector_t *vector);
 
-// Insert a new element at the end of the vector
-// Pre: 'vector' != NULL
 void vector_push(vector_t *vector, void *value);
 
-// Remove the last element in the vector and return the value
-// Pre: the vector is non-empty, 'vector' != NULL
 void *vector_pop(vector_t *vector);
 
-// Return the number of elements in the vector
-// Pre: 'vector' != NULL
 size_t vector_size(const vector_t *vector);
 
-// Return the current capacity of the vector
-// Pre: 'vector' != NULL
 size_t vector_capacity(const vector_t *vector);
 
-// Return the value at the given index
-// Pre: index < vector_size(vector)
 void *vector_get_element(const vector_t *vector, size_t index);
 
-// Return a pointer to the underlying array
 void **vector_get_array(const vector_t *vector);
 
 #endif
