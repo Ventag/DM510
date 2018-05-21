9233c5f592fe68e33a7d9a2e796b20e108eccbc7
Fred <Ventag>
2018-05-21 22:44:39 +0200

    Revision 1.2

--- lfs.c
+++ lfs.c
@@ -6,21 +6,13 @@
 #include "inode.h"
 
 int lfs_getattr(const char *, struct stat *);
-
 int lfs_readdir(const char *, void *, fuse_fill_dir_t, off_t, struct fuse_file_info *);
-
 int lfs_mknod (const char *, mode_t, dev_t);
-
 int lfs_mkdir(const char*, mode_t);
-
 int lfs_rmdir(const char*, mode_t);
-
 int lfs_open(const char *, struct fuse_file_info *);
-
 int lfs_read(const char *, char *, size_t, off_t, struct fuse_file_info *);
-
 int lfs_write(const char*, const char*, size_t, off_t, struct fuse_file_info *);
-
 int lfs_release(const char *path, struct fuse_file_info *fi);
 
 inode* root;
